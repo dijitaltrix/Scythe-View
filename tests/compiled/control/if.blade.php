8a2212c4c35d7b711716731c813d075a609c9654
Ian Grindley <ian.grindley@iperiumrealestate.com>
2018-03-10 18:43:28 +0000

    Added test to handle single and double quotes

--- tests/compiled/control/if.blade.php
+++ tests/compiled/control/if.blade.php
@@ -1,8 +1,14 @@
 @if ($kermit == 'frog')
 true
 @endif
+@if ($kermit != "frog")
+false
+@endif
 {{-- Expected
 <?php if ($kermit == 'frog'): ?>
 true
+<?php endif; ?>
+<?php if ($kermit != "frog"): ?>
+false
 <?php endif; ?>
 --}}
